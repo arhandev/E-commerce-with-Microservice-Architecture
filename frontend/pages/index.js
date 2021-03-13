7632a8165fb67eb2f528f09f312e44335f47afdd
supiraru <farhandewanta11@gmail.com>
2021-03-13 22:39:15 +0700

    initialize frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div className="">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="container mx-auto">
+                WEIII
+            </main>
+
+        </div>
+    );
+}
